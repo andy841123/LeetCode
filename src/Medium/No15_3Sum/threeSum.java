668465392d957e3af7bcd91235cd30f49e9cac31
Andy Chou <andy841123@gmail.com>
2021-04-20 11:34:35 +0800

    update No12 ver1.1

--- src/Medium/No15_3Sum/threeSum.java
+++ src/Medium/No15_3Sum/threeSum.java
@@ -2,6 +2,7 @@ package Medium.No15_3Sum;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,16 +21,18 @@ public class threeSum {
 
     static List<List<Integer>> three_sum(int[] nums) {
         List<List<Integer>> return_list = new ArrayList<>();
-
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) map.put(i, nums[i]);
         for (int i = 0; i < nums.length; i++){
             for (int j = i+1; j < nums.length; j++){
-                for (int k = j+1; k < nums.length; k++){
-                    if(nums[i] + nums[j] + nums[k] == 0){
-                        List<Integer> list = Arrays.asList(nums[i], nums[j], nums[k]);
-                        list.sort((s2,s1) -> -s1.compareTo(s2));
-                        return_list.add(list);
-                    } 
-                } 
+                HashMap<Integer, Integer> map_clone = (HashMap<Integer, Integer>) map.clone();
+                map_clone.remove(i);
+                map_clone.remove(j);
+                if (map_clone.containsValue(0-nums[i]-nums[j])){
+                    List<Integer> list = Arrays.asList(nums[i], nums[j], 0-nums[i]-nums[j]);
+                    list.sort((s2, s1) -> -s1.compareTo(s2));
+                    return_list.add(list);
+                }
             }
         }
         return return_list.stream().distinct().collect(Collectors.toList());
