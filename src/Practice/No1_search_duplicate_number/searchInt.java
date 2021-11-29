8db6711236de04d96a4c4576e46490c376af334c
andyycchou <AndyYCChou@fih-foxconn.com>
2021-11-29 12:16:41 +0800

    add practice No1

---
+++ src/Practice/No1_search_duplicate_number/searchInt.java
@@ -0,0 +1,19 @@
+package Practice.No1_search_duplicate_number;
+
+public class searchInt {
+    public static void main(String[] args) {
+        int [] target = {2,4,6,7,9,1,0,15,16,17,18,19,20,11,26,18};
+        System.out.print(mySearchDNum(target));
+    }
+
+    public static Integer mySearchDNum(int [] ary){
+        for (int i = 0; i < ary.length-1; i++){
+            for (int j = i+1; j < ary.length; j++){
+                if (ary[j] - ary[i] == 0){
+                    return ary[i];
+                }
+            }
+        }
+        return null;
+    }
+}
