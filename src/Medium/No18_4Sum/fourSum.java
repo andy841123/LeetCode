cd5415e5f24465799f123c817cef145b5b938ad1
andyycchou <AndyYCChou@fih-foxconn.com>
2021-11-30 18:11:32 +0800

    trim import function

--- src/Medium/No18_4Sum/fourSum.java
+++ src/Medium/No18_4Sum/fourSum.java
@@ -2,7 +2,6 @@ package Medium.No18_4Sum;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
