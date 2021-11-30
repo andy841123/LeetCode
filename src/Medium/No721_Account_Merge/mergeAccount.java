ec3e94772d77260911920409c4e309751a2147df
andyycchou <AndyYCChou@fih-foxconn.com>
2021-11-30 12:19:29 +0800

    union find 1st version

--- src/Medium/No721_Account_Merge/mergeAccount.java
+++ src/Medium/No721_Account_Merge/mergeAccount.java
@@ -2,6 +2,7 @@ package Medium.No721_Account_Merge;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.ListIterator;
 
@@ -19,15 +20,36 @@ public class mergeAccount {
     }
 
     public static List<List<String>> accountsMerge(List<List<String>> accounts) {
-        int [] root = new int [4];
+        HashMap<String, String> root_table = new HashMap<String, String>();
+        HashMap<String, String> name_table = new HashMap<String, String>();
 
-        for (int member = 0; member < accounts.size(); member++){
-            ListIterator<String> accIt = accounts.get(member).listIterator(1);
-            root[member] = member;
-            while(accIt.hasNext()){
-                System.out.println(accIt.next());
+        // 最左邊的account當作root
+        for (List<String> acc : accounts) {
+            ListIterator<String> emailIt = acc.listIterator(1);
+            String root_current = acc.get(1);
+            System.out.println(root_current);
+            String name = acc.get(0);
+            while(emailIt.hasNext()){
+                String acc_current = emailIt.next();
+                root_table.put(acc_current, root_current);
+                name_table.put(acc_current, name);
             }
         }
-        return null;
+
+        System.out.println(root_table);
+
+        ArrayList<List<String>> outAccount = new ArrayList<List<String>>();
+        for (String obj : root_table.keySet()) {
+            if (root_table.containsValue(obj)){
+                ArrayList<String> root_acc = new ArrayList<String>();
+                root_acc.add(name_table.get(obj));
+                for (String obj1 : root_table.keySet()){
+                    if (root_table.get(obj1).equals(obj) && !obj1.equals(obj)) root_acc.add(obj1);
+                }
+                root_acc.add(obj);
+                outAccount.add(root_acc);
+            } 
+        }
+        return outAccount;
     }
 }
